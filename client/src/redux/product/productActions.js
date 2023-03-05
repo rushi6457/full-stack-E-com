36ec507d733ae3edb907d92e91ae51fcf986c4f7
rushi6457 <rushiypatil@gmail.com>
2023-03-05 16:17:52 +0530

    done

--- client/src/redux/product/productActions.js
+++ client/src/redux/product/productActions.js
@@ -4,7 +4,7 @@ import { ADD_TO_CART, PRODUCT_ADD_FAIL, PRODUCT_ADD_REQUEST, PRODUCT_ADD_SUCCESS
 export const addProduct = (data) => async(dispatch) =>{
     dispatch({type:PRODUCT_ADD_REQUEST})
     try {
-        let res = await axios.post(`http://localhost:5000/admin/newproduct`,data)
+        let res = await axios.post(`https://e-com-78xd.onrender.com/admin/newproduct`,data)
         console.log(res.data);
         dispatch({type:PRODUCT_ADD_SUCCESS,payload:res.data})
     } catch (error) {
